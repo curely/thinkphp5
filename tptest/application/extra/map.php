30817e773b3032f3759c2c24d7d52c78b4606ea0
curely <15170057381@163.com>
2018-03-16 13:44:08 +0800

    Update map.php

--- tptest/application/extra/map.php
+++ tptest/application/extra/map.php
@@ -3,10 +3,10 @@
  * 地图相关配置文件
  */
 return [
-    'ak' => 'ggLxOfZVkCR4Fvuy2MOC18UMvWMxgEQY',
-    'map_url' => 'http://api.map.baidu.com/',
-    'geocoder' => 'geocoder/v2/',
-    'image' => 'staticimage/v2',
-    'width' => 400,
-    'height' => 300,
+    'ak' => '*****',
+    'map_url' => '*****',
+    'geocoder' => '*****',
+    'image' => '*****',
+    'width' => *****,
+    'height' => *****,
 ];
